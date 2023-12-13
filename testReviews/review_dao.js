015aba74f86fb3b163de548ae977c6e828ed22d0
Vamshika <lvamshika09@gmail.com>
2023-12-13 01:26:03 -0500

    add review

--- testReviews/review_dao.js
+++ testReviews/review_dao.js
@@ -1,3 +1,4 @@
 import test_review_model from "./review_model.js";
 
 export const findReviewsByMovieId = (movieId) => test_review_model.find({ movieId });
+export const createReview = (review) => test_review_model.create(review);
--- testReviews/review_routes.js
+++ testReviews/review_routes.js
@@ -1,13 +1,18 @@
 import * as reviewDao from "./review_dao.js";
 
 function TestReviewRoutes(app) {
-
   const findReviewsByMovieId = async (req, res) => {
     const reviews = await reviewDao.findReviewsByMovieId(req.params.movieId);
     res.json(reviews);
   };
 
+  const createReview = async (req, res) => {
+    const review = await reviewDao.createReview(req.body);
+    res.json(review);
+  };
+
   app.get("/api/testReviews/movie/:movieId", findReviewsByMovieId);
+  app.post("/api/testReviews/review", createReview);
 }
 
 export default TestReviewRoutes;
