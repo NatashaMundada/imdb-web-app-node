a2de9797b1a1315a80352b896bbcf4aeac28295f
Natasha Mundada <natashamundada@NatashaMundada.local>
2023-12-13 22:08:15 -0800

    follow api fix

--- follower_data/follow_dao.js
+++ follower_data/follow_dao.js
@@ -1,19 +1,27 @@
 import follow from "./follow_model.js";
 
 export const findAllFollowingUsers = (userId) => follow.findById(userId);
-export const followUser = (userId, userNameToFollow) => {
-    try {
-        let followData =  follow.findOne({ user: userId });
-        console.log(followData)
-        if (!followData) {
-          followData = new follow({ user: userId, following: [] });
-        }
-        if (!followData.following.includes(userNameToFollow)) {
-          followData.following.push(userNameToFollow);
-        followData.save();
-        }
-        return followData.following;
-      } catch (error) {
-            throw error;
-      }
-}
+export const followUser = async (userId, userNameToFollow) => {
+  try {
+    if (!userId) {
+      throw new Error('userId is required');
+    }
+
+    let followData = await follow.findOne({ userId: userId });
+    console.log(followData);
+
+    if (!followData) {
+      followData = new follow({ userId: userId, following: [] });
+    }
+
+    if (!followData.following.includes(userNameToFollow)) {
+      followData.following.push(userNameToFollow);
+      await followData.save();
+    }
+
+    return followData.following;
+  } catch (error) {
+    console.error('Error following user:', error);
+    throw error;
+  }
+};
